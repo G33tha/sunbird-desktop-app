8f78fa01c227c4ac6ceca4454e3a60d2dbc5dbf0
G33tha <geetha.p@optit.co>
2020-02-28 15:43:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 #Build 
 cd src
 npm install
-npm run build
+npm run build node --max_old_space_size=8000
 export LIMIT=12096
 npm install -g increase-memory-limit
 cd /temp/portal/src/app/client
-increase-memory-limit
+export NODE_OPTIONS=--max_old_space_size=4096
 npm run offline-prod 
 cd ../../../../../
 npm run build-copy-clean
